e27c3d812d1fff8b6bc380487719eb1a8ab90a33
PotassiumIodide <potassium.iodide28@gmail.com>
2020-12-11 22:35:12 +0900

    [Init] Initial Commit.

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.rustPlatform.buildRustPackage rec {
-  name = "hello";
+  name = "samplecli";
   version = "0.1.0";
   src = ./.;
   cargoSha256 = "1sksgrmhsirhmdrx7z4g2xix3h729i1w2c59fhsr617q6ch0n5ri";
--- shell.nix
+++ shell.nix
@@ -1,15 +1,13 @@
 let
   pkgs = import <nixpkgs> {};
-in
-  with pkgs;
   stdenv.mkDerivation {
-    name = "hello";
+    name = "samplecli";
     buildInputs = [
       cargo
       rustc
       pkgconfig
       openssl.dev
       nix
-      ];
+    ];
     OPENSSL_DEV=openssl.dev;
   }
